443f2e898cb00dc96b6f30d2b4f40fccf7e38fb1
Iord Neto <iordneto95@gmail.com>
2022-06-21 11:57:19 -0300

    add search components

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SearchBar = () => (
+  <div className="SearchBar">
+    <input placeholder="Enter A Song, Album, or Artist" />
+    <button className="SearchButton">SEARCH</button>
+  </div>
+);
+
+export default SearchBar;
---
+++ src/Components/SearchResults/SearchResults.jsx
@@ -0,0 +1,8 @@
+import React from "React";
+
+const SearchResults = () => (
+  <div className="SearchResults">
+    <h2>Results</h2>
+    {/*Add a TrackList component*/}
+  </div>
+);
