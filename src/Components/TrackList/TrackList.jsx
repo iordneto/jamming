4f3e123f5dd9429c11daa632126a6c54ed981b6f
Iord Neto <iordneto95@gmail.com>
2022-06-21 12:03:11 -0300

    add a tracklist component

---
+++ src/Components/TrackList/TrackList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TrackList = () => (
+  <div className="TrackList">
+    {/*<!-- You will add a map method that renders a set of Track components  -->*/}
+  </div>
+);
+
+export default TrackList;
