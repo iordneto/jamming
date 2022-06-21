71b6b8cefe04b98b788df2d358580ab486e77181
Iord Neto <iordneto95@gmail.com>
2022-06-21 12:01:31 -0300

    add a playlist component

---
+++ src/Components/Playlist/Playlist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Playlist = () => (
+  <div class="Playlist">
+    <input value="New Playlist" />
+    {/*<!-- Add a TrackList component -->*/}
+    <button class="Playlist-save">SAVE TO SPOTIFY</button>
+  </div>
+);
+
+export default Playlist;
